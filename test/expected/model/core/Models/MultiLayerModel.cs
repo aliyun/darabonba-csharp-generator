ebf62cdc9d39a2ca91bf917e67fb8393aaace358
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2021-03-04 15:51:57 +0800

    fix: emit sub-model lose some field when emit multi-layer model

---
+++ test/expected/model/core/Models/MultiLayerModel.cs
@@ -0,0 +1,50 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace Darabonba.Test.Models
+{
+    public class MultiLayerModel : TeaModel {
+        [NameInMap("Data")]
+        [Validation(Required=false)]
+        public MultiLayerModelData Data { get; set; }
+        public class MultiLayerModelData : TeaModel {
+            [NameInMap("Results")]
+            [Validation(Required=false)]
+            public List<MultiLayerModelDataResults> Results { get; set; }
+            public class MultiLayerModelDataResults : TeaModel {
+                public MultiLayerModelDataResultsTextRectangles TextRectangles { get; set; }
+                public class MultiLayerModelDataResultsTextRectangles : TeaModel {
+                    [NameInMap("Top")]
+                    [Validation(Required=false)]
+                    public int? Top { get; set; }
+
+                    [NameInMap("Width")]
+                    [Validation(Required=false)]
+                    public int? Width { get; set; }
+
+                    [NameInMap("Height")]
+                    [Validation(Required=false)]
+                    public int? Height { get; set; }
+
+                    [NameInMap("Angle")]
+                    [Validation(Required=false)]
+                    public int? Angle { get; set; }
+
+                    [NameInMap("Left")]
+                    [Validation(Required=false)]
+                    public int? Left { get; set; }
+
+                }
+                public string Text { get; set; }
+                public float? Probability { get; set; }
+            }
+        };
+
+    }
+
+}
