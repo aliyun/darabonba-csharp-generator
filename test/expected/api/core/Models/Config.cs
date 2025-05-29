0f5ff02086c78486ea763a11fd1aaa54f3e36568
zpp <865287328@qq.com>
2025-05-29 17:35:17 +0800

    fix: delete extra .Value of virtualVariable

--- test/expected/api/core/Models/Config.cs
+++ test/expected/api/core/Models/Config.cs
@@ -9,6 +9,16 @@ using Tea;
 namespace Darabonba.Test.Models
 {
     public class Config : TeaModel {
+        /// <summary>
+        /// <para>disable HTTP/2</para>
+        /// 
+        /// <b>Example:</b>
+        /// <para>false</para>
+        /// </summary>
+        [NameInMap("disableHttp2")]
+        [Validation(Required=false)]
+        public bool? DisableHttp2 { get; set; }
+
     }
 
 }
